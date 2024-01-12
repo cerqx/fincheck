cc3852157047fe634e0b418189fb09f66862d0d2
Cerqueira <contatocerq@gmail.com>
2024-01-12 17:56:30 -0300

    Login page styles has been initiated

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+
 export function Login() {
     return(
-        <h1>Login page</h1>
+        <div>
+            <header className="flex flex-col items-center gap-4">
+                <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+                    Entre em sua conta
+                </h1>
+
+                <p className="space-x-2">
+                    <span className="text-gray-700 tracking-[-0.5px]">
+                        Novo por aqui?
+                    </span>
+
+                    <Link 
+                        to="/register" 
+                        className="text-teal-900 tracking-[-0.5px] font-medium"
+                    >
+                        Crie uma conta.
+                    </Link>
+                </p>
+            </header>
+        </div>
     )
 }
