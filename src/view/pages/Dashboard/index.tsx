bb84a1506783767d94971bf14234035852f8b30b
Cerqueira <contatocerq@gmail.com>
2024-01-22 22:26:50 -0300

    Starting dashboard page and components

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -1,14 +1,19 @@
-import { useAuth } from "../../../app/hooks/useAuth";
-import { Button } from "../../components/Button";
+import { Logo } from "../../components/Logo";
+import { UserMenu } from "../../components/UserMenu";
 
 export function Dashboard() {
-    const { signout } = useAuth();
-
     return(
-        <div>
-            <h1>Dashboard page</h1>
+        <div className="h-full w-full px-8 pb-8 pt-6 flex flex-col gap-4">
+            <header className="h-12 flex items-center justify-between">
+                <Logo className="h-6 text-teal-900" />
 
-            <Button onClick={signout}>Sair</Button>
+                <UserMenu />
+            </header>
+
+            <main className="flex-1 flex gap-4">
+                <div className="w-1/2">Left</div>
+                <div className="w-1/2">Right</div>
+            </main>
         </div>
     )
 }
