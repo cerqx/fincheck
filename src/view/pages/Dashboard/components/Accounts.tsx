4cfac0fe48401d4ac5dfa6737105dbc47f659a89
Cerqueira <contatocerq@gmail.com>
2024-01-24 22:10:32 -0300

    Starting accounts style

--- src/view/pages/Dashboard/components/Accounts.tsx
+++ src/view/pages/Dashboard/components/Accounts.tsx
@@ -1,7 +1,37 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
+import { EyeIcon } from "../../../components/icons/EyeIcon";
+
 export function Accounts() {
     return (
-        <div className="bg-teal-900 rounded-2xl w-full h-full px-4 py-8 md:p-10">
-            Accounts
+        <div className="bg-teal-900 rounded-2xl w-full h-full px-4 py-8 md:p-10 flex flex-col">
+            <div>
+                <span className="tracking-[0.5px] text-white block">Saldo total</span>
+               
+               <div className="flex items-center gap-2">
+                    <strong className="text-2xl tracking-[-1px] text-white">R$ 1000,00</strong>
+                    <button className="h-8 w-8 flex items-center justify-center">
+                        <EyeIcon open/>
+                    </button>
+               </div>
+            </div>
+
+            <div className="flex-1 flex flex-col justify-end">
+                <div className="flex items-center justify-between">
+                    <strong className="text-white tracking-[-1px] text-lg font-bold">Minhas contas</strong>
+
+                    <div>
+                        <button className="py-3 pl-2.5 pr-3.5">
+                            <ChevronLeftIcon className="text-white w-6 h-6 rounded-full" />
+                        </button>
+
+                        <button className="py-3 pl-3.5 pr-2.5">
+                            <ChevronRightIcon className="text-white w-6 h-6 rounded-full"  />
+                        </button>
+                    </div>
+                </div>
+
+                <div>Contas...</div>
+            </div>
         </div>
     )
 }
