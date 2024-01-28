f310c273efdf79c4e2378a2afba367ad887f5ed3
Cerqueira <contatocerq@gmail.com>
2024-01-27 23:19:56 -0300

    Starting NewAccountModal form

--- src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,3 +1,5 @@
+import { Input } from "../../../../components/Input";
+import { InputCurrency } from "../../../../components/InputCurrency";
 import { Modal } from "../../../../components/Modal";
 import { useNewAccountModalController } from "./useNewAccountModalController";
 
@@ -10,7 +12,24 @@ export function NewAccountModal() {
             open={isNewAccountModalOpen}
             onClose={handleNewAccountModalClose}
         >
-            New Account Modal
+            <form>
+                <div>
+                    <span className="text-gray-600 text-xs  tracking-[-0.5px]">Saldo</span>
+
+                    <div className="flex items-center gap-2">
+                        <span className="text-gray-600 text-lg tracking-[-0.5px]">R$</span>
+                        <InputCurrency />
+                    </div>
+                </div>
+
+                <div className="mt-10">
+                    <Input 
+                        type="text" 
+                        name="name"
+                        placeholder="Nome da conta"
+                    />
+                </div>
+            </form>
         </Modal>
     )
 }
