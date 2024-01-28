61d0115942c525b539c4f395ba8191ebef86579e
Cerqueira <contatocerq@gmail.com>
2024-01-28 18:28:21 -0300

    Popover component done

--- src/view/components/DatePickerInput.tsx
+++ src/view/components/DatePickerInput.tsx
@@ -2,6 +2,7 @@ import { CrossCircledIcon } from "@radix-ui/react-icons";
 import { cn } from "../../app/utils/cn";
 import { useState } from "react";
 import { formatDate } from "../../app/utils/formatDate";
+import { Popover } from "./Popover";
 
 interface DatePickerInputProps {
     error?: string;
@@ -14,18 +15,26 @@ export function DatePickerInput({ className, error }: DatePickerInputProps) {
 
     return (
         <div>
-            <button 
-                type="button"
-                className={cn(
-                    "bg-white rounded-lg border border-gray-500 px-3 h-[52px] w-full text-gray-7 00 focus:border-gray-800 transition-all outline-none text-left pt-4 relative",
-                    error && '!border-red-900', //o ! na frente é igual ao !important no CSS, ou seja, ele tem prioridade
-                    className
-                )}
-            >
-                <span className="text-gray-700 text-xs absolute left-[13px] top-2 pointer-events-none">Data</span>
-                <span>{formatDate(selectedDate)}</span>
+            <Popover.Root>
+                <Popover.Trigger>
+                        <button 
+                            type="button"
+                            className={cn(
+                                "bg-white rounded-lg border border-gray-500 px-3 h-[52px] w-full text-gray-7 00 focus:border-gray-800 transition-all outline-none text-left pt-4 relative",
+                                error && '!border-red-900', //o ! na frente é igual ao !important no CSS, ou seja, ele tem prioridade
+                                className
+                            )}
+                        >
+                            <span className="text-gray-700 text-xs absolute left-[13px] top-2 pointer-events-none">Data</span>
+                            <span>{formatDate(selectedDate)}</span>
 
-            </button>
+                        </button>
+                </Popover.Trigger>
+
+                <Popover.Content className="z-[99]">
+                    Qualquer coisa
+                </Popover.Content>
+            </Popover.Root>
 
             {error && (
                 <div className="flex items-center mt-2 gap-2 text-red-900">
@@ -35,6 +44,7 @@ export function DatePickerInput({ className, error }: DatePickerInputProps) {
                     </span>
                 </div>
             )}
+
         </div>
     )
 }
